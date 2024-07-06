8e20b29b9f102ae68c3afde0cb4fd999f7184e90
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2024-07-06 15:14:53 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main()
+{
+	int even_sum=0,odd_sum=0;
+	pid_t pid=fork();
+	if(pid==0)
+	{
+		for(int i=2;i<=1000;i+=2)
+		{
+			even_sum+=i;
+		}
+	printf("Sum of even numbers is %d\n",even_sum);
+	}
+	
+	else if(pid<0)
+	{
+		printf("Fork failed\n");
+		return 1;
+	}
+	
+	else
+	{
+		for(int i=1;i<=1000;i+=2)
+		{
+			odd_sum+=i;
+		}
+		printf("Sum of odd numbers is %d\n",odd_sum);
+		wait(NULL);
+	}
+}
