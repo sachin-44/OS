70a2458497b3bdd1ab5c3ad5b2b2038563fa52db
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2024-07-07 18:39:58 +0530

    Create scandisk.c

---
+++ scandisk.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int requests[100];
+
+void sort(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n - i - 1; j++)
+        {
+            if(requests[j] > requests[j + 1])
+            {
+                int temp = requests[j];
+                requests[j] = requests[j + 1];
+                requests[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void SCAN(int n, int head, int direction, int size)
+{
+    int seek = 0;
+    sort(n);
+
+    int index;
+    for(index = 0; index < n; index++)
+    {
+        if(requests[index] > head)
+        {
+            break;
+        }
+    }
+
+    if(direction == 0) // Towards lower cylinder numbers
+    {
+        for(int i = index - 1; i >= 0; i--)
+        {
+            seek += abs(head - requests[i]);
+            head = requests[i];
+        }
+        seek += head;
+        head = 0;
+        for(int i = index; i < n; i++)
+        {
+            seek += abs(head - requests[i]);
+            head = requests[i];
+        }
+    }
+    else // Towards higher cylinder numbers
+    {
+        for(int i = index; i < n; i++)
+        {
+            seek += abs(head - requests[i]);
+            head = requests[i];
+        }
+        seek += size - 1 - head;
+        head = size - 1;
+        for(int i = index - 1; i >= 0; i--)
+        {
+            seek += abs(head - requests[i]);
+            head = requests[i];
+        }
+    }
+
+    printf("SCAN Total Seek Time: %d\n", seek);
+}
+
+int main()
+{
+    int n, head, size, direction;
+    printf("Enter the number of disk requests: ");
+    scanf("%d", &n);
+    printf("Enter the requests: ");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &requests[i]);
+    }
+    printf("Enter the initial head position: ");
+    scanf("%d", &head);
+    printf("Enter the size of the disk: ");
+    scanf("%d", &size);
+    printf("Enter direction (0: lower cylinder numbers, 1: higher cylinder numbers): ");
+    scanf("%d", &direction);
+
+    SCAN(n, head, direction, size);
+
+    return 0;
+}
