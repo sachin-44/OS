c956f2fbf79ac7afa8fa732d0feecffc50eae2ec
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2024-07-07 19:25:02 +0530

    Create semaphore.c

---
+++ semaphore.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<unistd.h>
+
+sem_t mutex;
+
+void *thread(void *arg)
+{
+	sem_wait(&mutex);
+	printf("\nEntered critical section\n");
+	sleep(4);
+	printf("\nExiting critical section\n");
+	sem_post(&mutex);
+	return NULL;
+}
+
+int main()
+{
+	sem_init(&mutex,0,1);
+	pthread_t t1,t2;
+	pthread_create(&t1,NULL,thread,NULL);
+	sleep(2);
+	pthread_create(&t2,NULL,thread,NULL);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	sem_destroy(&mutex);
+}
