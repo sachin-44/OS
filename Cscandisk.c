8ef7d55c8851aab748f2950c967cd271b1daa169
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2024-07-08 16:10:29 +0530

    Update Cscandisk.c

--- Cscandisk.c
+++ Cscandisk.c
@@ -1,94 +1,45 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int requests[100];
+int main() {
+    int r, re[10], sp, ds, thm = 0, num = 0;
 
-void sort(int n)
-{
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n - i - 1; j++)
-        {
-            if(requests[j] > requests[j + 1])
-            {
-                int temp = requests[j];
-                requests[j] = requests[j + 1];
-                requests[j + 1] = temp;
+    printf("Enter the Number of Requests:\t");
+    scanf("%d", &r);
+
+    printf("Enter the Request Sequence:\n");
+    for (int i = 0; i < r; i++) {
+        scanf("%d", &re[i]);
+    }
+
+    printf("Enter the Starting Point:\t");
+    scanf("%d", &sp);
+
+    printf("Enter the Disk Size:\t");
+    scanf("%d", &ds);
+
+    // Sort request sequence (optional, for better understanding)
+    for (int i = 0; i < r - 1; i++) {
+        for (int j = 0; j < r - i - 1; j++) {
+            if (re[j] > re[j + 1]) {
+                int temp = re[j];
+                re[j] = re[j + 1];
+                re[j + 1] = temp;
             }
         }
     }
-}
 
-void C_SCAN(int n, int head, int direction, int size)
-{
-    int seek = 0;
-    sort(n);
-
-    int index;
-    for(index = 0; index < n; index++)
-    {
-        if(requests[index] > head)
-        {
-            break;
+    // Find the nearest request in the opposite direction
+    for (int i = 0; i < r; i++) {
+        if (re[i] < sp) {
+            num = re[i];
         }
     }
 
-    if(direction == 0) // Towards lower cylinder numbers
-    {
-        for(int i = index; i < n; i++)
-        {
-            seek += abs(head - requests[i]);
-            head = requests[i];
-        }
-        seek += size - 1 - head;
-        head = size - 1;
-        seek += head;
-        head = 0;
-        for(int i = 0; i < index; i++)
-        {
-            seek += abs(head - requests[i]);
-            head = requests[i];
-        }
-    }
-    else // Towards higher cylinder numbers
-    {
-        for(int i = index - 1; i >= 0; i--)
-        {
-            seek += abs(head - requests[i]);
-            head = requests[i];
-        }
-        seek += head;
-        head = 0;
-        seek += size - 1 - head;
-        head = size - 1;
-        for(int i = n - 1; i >= index; i--)
-        {
-            seek += abs(head - requests[i]);
-            head = requests[i];
-        }
-    }
+    // Calculate total head movements in CSCAN
+    thm += (ds - 1 - sp) + (ds - 1) + (num - 0);
 
-    printf("C-SCAN Total Seek Time: %d\n", seek);
-}
-
-int main()
-{
-    int n, head, size, direction;
-    printf("Enter the number of disk requests: ");
-    scanf("%d", &n);
-    printf("Enter the requests: ");
-    for(int i = 0; i < n; i++)
-    {
-        scanf("%d", &requests[i]);
-    }
-    printf("Enter the initial head position: ");
-    scanf("%d", &head);
-    printf("Enter the size of the disk: ");
-    scanf("%d", &size);
-    printf("Enter direction (0: lower cylinder numbers, 1: higher cylinder numbers): ");
-    scanf("%d", &direction);
-
-    C_SCAN(n, head, direction, size);
+    printf("Total Head Movements in CSCAN: %d\n", thm);
 
     return 0;
 }
