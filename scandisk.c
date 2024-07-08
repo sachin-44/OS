81542304ad866ffcbb5229273e9bf61167f44c6e
sachin-44 <145382967+sachin-44@users.noreply.github.com>
2024-07-08 15:55:59 +0530

    Update scandisk.c

--- scandisk.c
+++ scandisk.c
@@ -1,90 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
-int requests[100];
+int main() {
+    int r, re[10], sp, ds, thm = 0;
 
-void sort(int n)
-{
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n - i - 1; j++)
-        {
-            if(requests[j] > requests[j + 1])
-            {
-                int temp = requests[j];
-                requests[j] = requests[j + 1];
-                requests[j + 1] = temp;
+    printf("Enter the Number of Requests:\t");
+    scanf("%d", &r);
+
+    printf("Enter the Request Sequence:\n");
+    for (int i = 0; i < r; i++) {
+        scanf("%d", &re[i]);
+    }
+
+    printf("Enter the Starting Point:\t");
+    scanf("%d", &sp);
+
+    printf("Enter the Disk Size:\t");
+    scanf("%d", &ds);
+
+    for (int i = 0; i < r - 1; i++) {
+        for (int j = 0; j < r - i - 1; j++) {
+            if (re[j] > re[j + 1]) {
+                int temp = re[j];
+                re[j] = re[j + 1];
+                re[j + 1] = temp;
             }
         }
     }
-}
 
-void SCAN(int n, int head, int direction, int size)
-{
-    int seek = 0;
-    sort(n);
-
-    int index;
-    for(index = 0; index < n; index++)
-    {
-        if(requests[index] > head)
-        {
-            break;
-        }
-    }
-
-    if(direction == 0) // Towards lower cylinder numbers
-    {
-        for(int i = index - 1; i >= 0; i--)
-        {
-            seek += abs(head - requests[i]);
-            head = requests[i];
-        }
-        seek += head;
-        head = 0;
-        for(int i = index; i < n; i++)
-        {
-            seek += abs(head - requests[i]);
-            head = requests[i];
-        }
-    }
-    else // Towards higher cylinder numbers
-    {
-        for(int i = index; i < n; i++)
-        {
-            seek += abs(head - requests[i]);
-            head = requests[i];
-        }
-        seek += size - 1 - head;
-        head = size - 1;
-        for(int i = index - 1; i >= 0; i--)
-        {
-            seek += abs(head - requests[i]);
-            head = requests[i];
-        }
-    }
-
-    printf("SCAN Total Seek Time: %d\n", seek);
-}
-
-int main()
-{
-    int n, head, size, direction;
-    printf("Enter the number of disk requests: ");
-    scanf("%d", &n);
-    printf("Enter the requests: ");
-    for(int i = 0; i < n; i++)
-    {
-        scanf("%d", &requests[i]);
-    }
-    printf("Enter the initial head position: ");
-    scanf("%d", &head);
-    printf("Enter the size of the disk: ");
-    scanf("%d", &size);
-    printf("Enter direction (0: lower cylinder numbers, 1: higher cylinder numbers): ");
-    scanf("%d", &direction);
-
-    SCAN(n, head, direction, size);
+    thm += ((ds - 1) - sp) + ((ds - 1) - re[0]);
+    printf("Total Head Movements in SCAN: %d\n", thm);
 
     return 0;
 }
